e8bfb3dd89547b42696d2d5ed6742436be662dae
Sea Sun <sunsealucky@gmail.com>
2023-11-15 19:43:19 +0800

    update 2011

--- main.py
+++ main.py
@@ -10,24 +10,31 @@ PATHS = {
     "2015": {
         "HSF_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2015/Data/Health_Status_and_Functioning.dta",
         "DB_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2015/Data/Demographic_Background.dta",
+        "BIO_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2015/Data/Biomarker.dta",
     },
     "2013": {
         "HSF_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2013/Data/Health_Status_and_Functioning.dta",
         "DB_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2013/Data/Demographic_Background.dta",
+        "BIO_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2013/Data/Biomarker.dta",
     },
     "2011": {
         "HSF_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2011/Data/health_status_and_functioning.dta",
         "DB_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2011/Data/demographic_background.dta",
+        "BIO_PATH": "D:/Personal/University/CollegeProject/CollegeProject-2/CHARLS/CHARLS-Database-2011/Data/biomarkers.dta",
     },
 }
 
 # 2011 -----
 a = pd.read_stata(PATHS["2011"]["HSF_PATH"], index_col="ID", convert_categoricals=False)
 b = pd.read_stata(PATHS["2011"]["DB_PATH"], index_col="ID", convert_categoricals=False)
-y2011_resource = a.merge(b, on="ID")
+c = pd.read_stata(PATHS["2011"]["BIO_PATH"], index_col="ID", convert_categoricals=False)
+y2011_resource = c.merge(a.merge(b, on="ID"), on="ID")
 
-y2011 = Operator.DataOperator(2011, "MMSE", y2011_resource)
+y2011_cog = Operator.DataOperator(2011, "MMSE", y2011_resource)
+y2011_fra = Operator.DataOperator(2011, "PFP", y2011_resource)
 
-cog = y2011.calculate_MMSE_indicators()
+y2011_cog.calculate_MMSE_indicators()
+y2011_fra.calculate_PFP_indicators()
 
-y2011.df.to_csv("1.csv")
+y2011_cog.df.to_csv("cog.csv")
+y2011_fra.df.to_csv("fra.csv")
