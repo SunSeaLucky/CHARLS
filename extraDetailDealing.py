1a219556dc4b1abc420bd592950341779b700aaf
Sea Sun <sunsealucky@gmail.com>
2023-12-30 01:35:28 +0800

    FIXED extraDetialDealing.py problem

--- extraDetailDealing.py
+++ extraDetailDealing.py
@@ -7,7 +7,11 @@ df_cog_only_15_array = []
 
 for index, row in df.fillna(-1).iterrows():
     # print(row[4], row[6], row[8], index)
-    if int(row[4]) == 1 and int(row[6]) != 1 and int(row[8]) != 1:
+    if (
+        int(row["cognition_impairment_2015"]) == 1
+        and int(row["cognition_impairment_2013"]) != 1
+        and int(row["cognition_impairment_2011"]) != 1
+    ):
         df_cog_only_15_array.append(index)
 # print(df_cog_only_15_array)
 df.loc[df_cog_only_15_array].to_csv("Result/cog_only_15.csv")
@@ -16,7 +20,11 @@ df.loc[df_cog_only_15_array].to_csv("Result/cog_only_15.csv")
 df_cog_never_array = []
 
 for index, row in df.fillna(-1).iterrows():
-    if int(row[4]) != 1 and int(row[6]) != 1 and int(row[8]) != 1:
+    if (
+        int(row["cognition_impairment_2015"]) != 1
+        and int(row["cognition_impairment_2013"]) != 1
+        and int(row["cognition_impairment_2011"]) != 1
+    ):
         df_cog_never_array.append(index)
 
 df.loc[df_cog_never_array].to_csv("Result/cog_never.csv")
